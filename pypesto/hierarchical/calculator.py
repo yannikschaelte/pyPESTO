82cc1c5783ec1a60a68f7d3cf7b1a14287626df9
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-19 07:17:37 +0100

    fix multiple errors

--- pypesto/hierarchical/calculator.py
+++ pypesto/hierarchical/calculator.py
@@ -38,6 +38,7 @@ class HierarchicalForwardAmiciCalculator(HierarchicalAmiciCalculator):
         sres = np.zeros([0, obj.dim])
 
         # set order in solver
+        print("sensi_order: ", sensi_order)
         obj.amici_solver.setSensitivityOrder(sensi_order)
 
         # run amici simulation
@@ -47,7 +48,7 @@ class HierarchicalForwardAmiciCalculator(HierarchicalAmiciCalculator):
             obj.edatas,
             num_threads=min(obj.n_threads, len(obj.edatas)),
         )
-
+        #print(rdatas[0])
         if any([rdata['status'] < 0.0 for rdata in rdatas]):
             return obj.get_error_output(rdatas)
 
@@ -58,43 +59,13 @@ class HierarchicalForwardAmiciCalculator(HierarchicalAmiciCalculator):
 
         # compute optimal parameters
         optimal_scalings = compute_optimal_scaling_matrix(self.problem, edatas, rdatas)
-        
-        # compute likelihood and derivatives
-        for data_ix, rdata in enumerate(rdatas):
-            log_simulation(data_ix, rdata)
 
-            # compute objective
-            if mode == MODE_FUN:
-                nllh -= rdata['llh']
-                if sensi_order > 0:
-                    add_sim_grad_to_opt_grad(
-                        obj.x_ids,
-                        obj.mapping_par_opt_to_par_sim[data_ix],
-                        rdata['sllh'],
-                        snllh,
-                        coefficient=-1.0
-                    )
-                    # TODO: Compute the full Hessian, and check here
-                    add_sim_hess_to_opt_hess(
-                        obj.x_ids,
-                        obj.mapping_par_opt_to_par_sim[data_ix],
-                        rdata['FIM'],
-                        s2nllh,
-                        coefficient=-1.0
-                    )
+        nllh = compute_nllh(edatas, rdatas, optimal_scalings)
+        if sensi_order > 0:
+            snllh = compute_snllh(edatas, rdatas, optimal_scalings, obj.x_ids, obj.mapping_par_opt_to_par_sim)
 
-            elif mode == MODE_RES:
-                res = np.hstack([res, rdata['res']]) \
-                    if res.size else rdata['res']
-                if sensi_order > 0:
-                    opt_sres = sim_sres_to_opt_sres(
-                        obj.x_ids,
-                        obj.mapping_par_opt_to_par_sim[data_ix],
-                        rdata['sres'],
-                        coefficient=1.0
-                    )
-                    sres = np.vstack([sres, opt_sres]) \
-                        if sres.size else opt_sres
+        # TODO compute FIM or HESS
+        # TODO RES, SRES should also be possible, right?
 
         return {
             FVAL: nllh,
@@ -127,25 +98,27 @@ def compute_optimal_scalings(problem, edatas, rdatas):
     for x in problem.get_xs_for_type(HierarchicalParameter.SCALING):
         s_opt = compute_optimal_scaling(x, edatas, rdatas)
         optimal_scalings.append(s_opt)
+    return optimal_scalings
 
 
 def compute_optimal_scaling(x, edatas, rdatas):
     num = 0.0
     den = 0.0
 
-    for ix in x.iterate():
-        condition_ix, time_ix, observable_ix = ix
+    for condition_ix, time_ix, observable_ix in x.iterate():
+        #print(condition_ix, time_ix, observable_ix)
         y = edatas[condition_ix]['observedData'][time_ix, observable_ix]
         h = rdatas[condition_ix]['y'][time_ix, observable_ix]
-
-        if np.isnan(y) or np.isnan(h):
+        sigma = rdatas[condition_ix]['sigmay'][time_ix, observable_ix]
+        #print(y, h, sigma)
+        if np.isnan(y) or np.isnan(h) or np.isnan(sigma):
             continue
 
-        num += (y - h) * h
-        den += h**2
+        num += (y - h) * h / sigma**2
+        den += h**2 / sigma**2
 
     # TODO check for 0.0
-
+    #print("num, den: ", num, den, num/den)
     return num / den
 
 
@@ -164,3 +137,26 @@ def apply_optimal_scalings(problem, optimal_scalings, matrix):
 def apply_optimal_scaling(x, opt_s, matrix):
     for condition_ix, time_ix, observable_ix in x.iterate():
         matrix[condition_ix][time_ix, observable_ix] = opt_s
+
+
+def compute_nllh(edatas, rdatas, optimal_scalings):
+    nllh = 0.0
+    for edata, rdata, optimal_s in zip(edatas, rdatas, optimal_scalings):
+        nllh += 0.5 * np.nansum((edata['observedData'] - optimal_s * rdata['y'])**2 / rdata['sigmay']**2)
+    print(nllh)
+    return nllh
+
+
+def compute_snllh(edatas, rdatas, optimal_scalings, x_ids, mapping_par_opt_to_par_sim):
+    snllh = 0.0
+    for condition_ix, (edata, rdata, optimal_s) in \
+            enumerate(zip(edatas, rdatas, optimal_scalings)):
+        val = np.nansum((edata['observedData'] - optimal_s * rdata['y']) \
+                / rdata['sigmay']**2) * optimal_s * rdata['sy']
+        add_sim_grad_to_opt_grad(
+            x_ids,
+            mapping_par_opt_to_par_sim[condition_ix],
+            val,
+            snllh,
+            coefficient=1.0)
+    return snllh
